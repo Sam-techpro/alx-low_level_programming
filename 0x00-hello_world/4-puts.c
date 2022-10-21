fc68eae15e25905cb6d1bbb2af6283d7bb45f9d4
Sam-techpro <sammietee218@gmail.com>
2022-10-20 18:03:04 -0700

    Hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - print a particular text
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char str[]= "Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
